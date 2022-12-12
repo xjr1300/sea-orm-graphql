24c81011008d22f44d7abf8171be59f15964cb48
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-12-12 18:27:43 +0900

    基本的なCRUD操作を実装

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,63 @@
 use std::env;
 
 use dotenvy::dotenv;
-use sea_orm::{Database, DbErr};
+use sea_orm::{ActiveValue, Database, DbErr, *};
+
+mod entities;
+
+use entities::{prelude::*, *};
 
 async fn run(postgres_url: &str, database_name: &str) -> Result<(), DbErr> {
     let url = format!("{}/{}", postgres_url, database_name);
-    let _db = Database::connect(&url).await?;
+    let db = Database::connect(&url).await?;
+
+    let happy_bakery = bakery::ActiveModel {
+        name: ActiveValue::Set(String::from("Happy Bakery")),
+        profit_margin: ActiveValue::Set(0.0),
+        ..Default::default()
+    };
+    let result = Bakery::insert(happy_bakery).exec(&db).await?;
+
+    let sad_bakery = bakery::ActiveModel {
+        id: ActiveValue::Set(result.last_insert_id),
+        name: ActiveValue::Set(String::from("Sad Bakery")),
+        profit_margin: ActiveValue::NotSet,
+    };
+    sad_bakery.update(&db).await?;
+
+    let john = chef::ActiveModel {
+        name: ActiveValue::Set(String::from("John")),
+        bakery_id: ActiveValue::Set(result.last_insert_id),
+        ..Default::default()
+    };
+    Chef::insert(john).exec(&db).await?;
+
+    let bakeries = Bakery::find().all(&db).await?;
+    assert_eq!(bakeries.len(), 1);
+
+    let sad_bakery = Bakery::find_by_id(1).one(&db).await?;
+    assert_eq!(sad_bakery.unwrap().id, 1);
+
+    let sad_bakery = Bakery::find()
+        .filter(bakery::Column::Name.eq("Sad Bakery"))
+        .one(&db)
+        .await?;
+    assert_eq!(sad_bakery.unwrap().id, 1);
+
+    let john = chef::ActiveModel {
+        id: ActiveValue::Set(1),
+        ..Default::default()
+    };
+    john.delete(&db).await?;
+
+    let sad_bakery = bakery::ActiveModel {
+        id: ActiveValue::Set(1),
+        ..Default::default()
+    };
+    sad_bakery.delete(&db).await?;
+
+    let bakeries = Bakery::find().all(&db).await?;
+    assert!(bakeries.is_empty());
 
     Ok(())
 }
