00abbeed41075b4bda6c2ebf19346034f44162a7
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-12-12 16:41:52 +0900

    postgresに接続するコードを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenvy::dotenv;
+use sea_orm::{Database, DbErr};
+
+async fn run(postgres_url: &str) -> Result<(), DbErr> {
+    let _db = Database::connect(postgres_url).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().expect(".env file not found");
+
+    let postgres_url = env::var("POSTGRES_URL").expect("POSTGRES_URL not found in .env file");
+    if let Err(e) = run(&postgres_url).await {
+        panic!("{}", e);
+    }
 }
