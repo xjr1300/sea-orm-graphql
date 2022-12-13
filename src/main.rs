d365bf98c8013e8366c45065078f4e32833f926a
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-12-13 09:27:17 +0900

    コードを整理

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,14 @@ mod entities;
 
 use entities::{prelude::*, *};
 
-async fn run(postgres_url: &str, database_name: &str) -> Result<(), DbErr> {
-    let url = format!("{}/{}", postgres_url, database_name);
-    let db = Database::connect(&url).await?;
 
-    delete_records(&db).await?;
+async fn run_sea_orm(db: &DatabaseConnection) -> Result<(), DbErr> {
+    delete_records(db).await?;
 
-    basic_crud_operations(&db).await?;
-    relationship_select(&db).await?;
+    basic_crud_operations(db).await?;
+    relationship_select(db).await?;
     test_with_mock().await?;
-    build_sea_queries(&db).await?;
+    build_sea_queries(db).await?;
 
     Ok(())
 }
@@ -287,15 +285,20 @@ async fn build_sea_queries(db: &DatabaseConnection) -> Result<(), DbErr> {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     dotenv().expect(".env file not found");
 
     let postgres_url = env::var("POSTGRES_URL").expect("POSTGRES_URL not found in .env file");
     let database_name =
         env::var("POSTGRES_DATABASE").expect("POSTGRES_DATABASE not found in .env file");
-    if let Err(e) = run(&postgres_url, &database_name).await {
+    let url = format!("{}/{}", postgres_url, database_name);
+    let db = Database::connect(&url).await?;
+
+    if let Err(e) = run_sea_orm(&db).await {
         panic!("{}", e);
     }
+
+    Ok(())
 }
 
 #[derive(FromQueryResult)]
